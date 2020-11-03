ca3b2e76ca277e6fc0a1c208189ee3d9a485ddaa
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-03 15:47:09 +0530

    [mrunal] Add. Added features to connect to database and retrieve and add data to EmployeePayroll table

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll !");
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+
+            employee.Name = "Terrisa";
+            employee.Salary = 15000;
+            employee.Gender = 'F';
+            employee.PhoneNumber = "7596568964";
+            employee.Address = "Nagpur";
+            employee.Department = "R&D";
+            employee.BasicPay = 1000;
+            employee.Deductions = 100;
+            employee.TaxablePay = 0;
+            employee.IncomeTax = 0;
+            employee.NetPay = 900;
+            employeeRepo.AddEmployee(employee);
+            //employeeRepo.GetEmployee();
+        }
+    }
+}
