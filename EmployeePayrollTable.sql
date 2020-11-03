2f88cb297f4cf3474f86dd52697c39cfeaa305ce
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-03 15:38:18 +0530

    [mrunal] Add. Implemented ER diagram into payroll service db and completed uc11, uc12

--- EmployeePayrollTable.sql
+++ EmployeePayrollTable.sql
@@ -27,7 +27,7 @@ update employee_payroll set Gender='F' where Name = 'Mrunal' or Name='Sam';
 update employee_payroll set Gender='M' where Name = 'Abc' or Name='John';
 
 -- UC 7
-select SUM(Salary) from employee_payroll where Gender='F' group by Gender;
+select SUM(Salary) as TotalSalary from employee_payroll where Gender='F' group by Gender;
 select AVG(Salary) from employee_payroll where Gender='M' group by Gender;
 select COUNT(ID) from employee_payroll where Gender='F' group by Gender;
 
@@ -38,6 +38,8 @@ alter table employee_payroll add Department varchar(30) NOT NULL default '';
 
 update employee_payroll set PhoneNumber='9854785965' where Name = 'Mrunal';
 update employee_payroll set PhoneNumber='7854785965' where Name = 'John';
+update employee_payroll set PhoneNumber='8596541526' where Name='Abc';
+update employee_payroll set PhoneNumber='7896541526' where Name='Sam';
 
 update employee_payroll set Department='HR' where Name = 'Mrunal' or Name='John';
 update employee_payroll set Department='R&D' where Name = 'Sam';
@@ -50,21 +52,85 @@ alter table employee_payroll add TaxablePay decimal(10, 2) NOT NULL default 0;
 alter table employee_payroll add IncomeTax decimal(10, 2) NOT NULL default 0;
 alter table employee_payroll add NetPay decimal(10, 2) NOT NULL default 0;
 
+-- UC 11
+create table company(
+Comp_ID int NOT NULL identity(1, 1) PRIMARY KEY,
+Comp_Name varchar(50) NOT NULL,
+);
 
---update employee_payroll set BasicPay=20000 where Name='Mrunal' or Name='John';
---update employee_payroll set BasicPay=30000 where Name='Abc' or Name='Sam';
+create table payroll(
+Payroll_ID int NOT NULL identity(1, 1) PRIMARY KEY,
+BasicPay decimal(10,2) NOT NULL,
+Deductions decimal(10,2) NOT NULL,
+TaxablePay as (BasicPay-Deductions),
+IncomeTax decimal(10,2) NOT NULL,
+NetPay as ((BasicPay-Deductions)-IncomeTax),
+StartDate datetime NOT NULL
+);
 
---update employee_payroll set Deductions=2000 where Name='Mrunal' or Name='Abc';
---update employee_payroll set Deductions=1500 where Name='John';
---update employee_payroll set Deductions=3000 where Name='Sam';
+create table employee(
+Emp_ID int NOT NULL identity(1000, 1) PRIMARY KEY,
+Emp_Name varchar(50) NOT NULL,
+Emp_Phone varchar(50) NOT NULL,
+Emp_Address varchar(50) NOT NULL,
+Emp_Gender varchar(50) NOT NULL,
+Comp_ID int FOREIGN KEY REFERENCES company(Comp_ID),
+Payroll_ID int FOREIGN KEY REFERENCES payroll(Payroll_ID),
+);
 
---update employee_payroll set TaxablePay=1000;
+create table department(
+Dept_ID int NOT NULL identity(10, 1) PRIMARY KEY,
+Dept_Name varchar(50) NOT NULL
+);
 
---update employee_payroll set IncomeTax=200;
+create table emp_dept(
+Emp_ID int FOREIGN KEY REFERENCES employee(Emp_ID),
+Dept_ID int FOREIGN KEY REFERENCES department(Dept_ID),
+PRIMARY KEY (Emp_ID,Dept_ID)
+);
 
---update employee_payroll set NetPay=18000 where Name='Mrunal';
---update employee_payroll set NetPay=28000 where Name='Abc';
---update employee_payroll set NetPay=27000 where Name='Sam';
---update employee_payroll set NetPay=15000 where Name='John';
+select * from company;
+select * from payroll;
+select * from employee;
+select * from department;
+select * from emp_dept;
 
--- UC 10
+insert into company values('Capgemini');
+insert into company values('TCS');
+
+insert into payroll values(2000, 200, 50, '2020-03-01');
+insert into payroll values(4000, 250, 100, '2020-02-01');
+insert into payroll values(3000, 150, 70, '2020-05-01');
+
+insert into employee values('Mrunal', '8596515265', 'LA', 'F', 1, 2);
+insert into employee values('John', '9999515265', 'SA', 'M', 2, 1);
+insert into employee values('Sam', '8899515265', 'LA', 'F', 1, 3);
+
+
+insert into department values('HR');
+insert into department values('R&D');
+insert into department values('Marketing');
+
+
+insert into emp_dept values(1000,10);
+insert into emp_dept values(1001,10);
+insert into emp_dept values(1001,12);
+insert into emp_dept values(1002,11);
+
+-- UC 12
+select Emp_ID, Emp_Name, Emp_Phone, Emp_Address, Emp_Gender, Emp_Phone, BasicPay, Deductions, TaxablePay, IncomeTax, NetPay
+from employee inner join payroll
+on employee.Payroll_ID = payroll.Payroll_ID;
+
+select * from payroll where Payroll_ID = (select Payroll_ID from employee where Emp_Name='Mrunal');
+
+select Emp_ID, Emp_Name, Emp_Phone, Emp_Address, Emp_Gender, Emp_Phone, BasicPay, Deductions, TaxablePay, IncomeTax, NetPay
+from employee inner join payroll
+on employee.Payroll_ID = payroll.Payroll_ID
+where StartDate between CAST('2020-03-01' AS DATE) AND GETDATE();
+
+select SUM(NetPay) as Total_Salary, COUNT(payroll.Payroll_ID) as Total_Count
+from payroll inner join employee
+on employee.Payroll_ID = payroll.Payroll_ID
+group by employee.Emp_Gender;
+
